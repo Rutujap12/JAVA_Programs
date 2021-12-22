06b72475857519a265d9d72912b796231dd3117f
Rutujap12 <96284900+Rutujap12@users.noreply.github.com>
2021-12-22 10:41:50 +0530

    Add files via upload

---
+++ ArrayProgram.java
@@ -0,0 +1,11 @@
+public class ArrayProgram {
+    public static void main(String[] args) {
+        int[] elements = {2, 16, 40, 50, 20};
+        System.out.println("Elements in Array");
+        System.out.println("1st Element: " + elements[0]);
+        System.out.println("2nd Element: " + elements[1]);
+        System.out.println("3rd Element: " + elements[2]);
+        System.out.println("4th Element: " + elements[3]);
+        System.out.println("5th Element: " + elements[4]);
+    }
+}
